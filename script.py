e5812a8c61bfa273f3163604156392680fa599ec
Saeed Khan <saeed786129@hotmail.com>
2020-06-06 14:07:15 +0100

    Now manages directories for each manga

--- script.py
+++ script.py
@@ -4,12 +4,27 @@ import urllib.request
 import shutil 
 import os
 
+root_folder = "MyManga"
+
+try:
+	os.mkdir(root_folder);
+	print("Directory " , root_folder ,  " Created ") 
+except FileExistsError:
+	print("Directory", root_folder, " already exits")
 #number of chapters
 no_of_chaps = 100
 #number of pages in chapter
-max_no_of_pages = 50
+max_no_of_pages = 60
 #manga that you want to get
-manga_name = "kimetsu-no-yaiba"
+manga_name = "shokugeki-no-soma"
+
+manga_folder = root_folder + '/' + manga_name
+
+try:
+	os.mkdir(manga_folder);
+	print("Directory " , manga_folder ,  " Created ") 
+except FileExistsError:
+	print("Directory", manga_folder, " already exits")
 
 
 for i in range(no_of_chaps):
@@ -19,7 +34,7 @@ for i in range(no_of_chaps):
 	 
 	try:
 	    # Create a new folder
-	    os.mkdir(folder_name)
+	    os.mkdir(manga_folder + '/' +folder_name)
 	    print("Directory " , folder_name ,  " Created ") 
 	except FileExistsError:
 	    print("Directory " , folder_name ,  " already exists")
@@ -33,7 +48,7 @@ for i in range(no_of_chaps):
 			chap_page = x+1
 			url = 'http://www.mangapanda.com/' + manga_name + '/' + str(index) + '/' +str(chap_page)
 		
-		#
+		
 		req = requests.get(url)
 		if req.status_code == 404:
 			break
@@ -44,7 +59,7 @@ for i in range(no_of_chaps):
 		#gets the image url
 		image_url = images['src']
 
-		filename = str(chap_page) + image_url.split("/")[-1] 
+		filename = str(chap_page) + '_' + image_url.split("/")[-1] 
 
 		# Open the url image, set stream to True, this will return the stream content.
 		r = requests.get(image_url, stream = True)
@@ -55,8 +70,7 @@ for i in range(no_of_chaps):
 		    r.raw.decode_content = True
 		    
 		    # Open a local file with wb ( write binary ) permission.
-		    with open(folder_name + '/'+filename,'wb') as f:
-		        shutil.copyfileobj(r.raw, f)
+		    with open(manga_folder + '/' +folder_name + '/'+filename,'wb') as f: shutil.copyfileobj(r.raw, f)
 		        
 		    print('Image sucessfully Downloaded: ',filename)
 		else:
